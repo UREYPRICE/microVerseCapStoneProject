6dfec9220f054571eed11b20dfae3a8b40737099
root <uchihawa@gmail.com>
2022-10-20 16:09:15 +0500

    Added desktop view to about page

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  const x = document.querySelector('.mobile-menu');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+myFunction();
